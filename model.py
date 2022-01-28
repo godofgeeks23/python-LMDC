c09c29f7fbb5941c25450d7e9b545287aaaf7950
Aviral Srivastava <aviralji4@gmail.com>
2022-01-29 00:03:44 +0530

    Feature selection

--- model.py
+++ model.py
@@ -1,15 +1,18 @@
 from fileinput import filename
 from tkinter.tix import Y_REGION
+from matplotlib.transforms import Bbox
 import pandas as pd
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import missingno as msno
 from sklearn.ensemble import ExtraTreesClassifier
+from sklearn import preprocessing
 
 filename = 'summary_mod2.csv'
 df = pd.read_csv(filename)
-print(df.shape)
+
+# print(df.shape)
 # msno.matrix(df).get_figure().savefig('newchart2.png')
 
 # print(df.head)
@@ -17,25 +20,26 @@ df = df.drop(['file_name'], axis=1)
 
 features = df.columns.values
 data = df[features[:-1]]
+
 target = df[features[-1]]
 
 classes = target.value_counts()
 print(classes)
 
-height = list(classes)
-bars = ('benign', 'ddos', 'backdoor', 'botnet', 'virus', 'trojan')
-y_pos = np.arange(len(bars))
-plt.bar(y_pos, height, color=['green', 'red', 'blue', 'yellow', 'orange', 'purple'])
-plt.xticks(y_pos, bars)
-plt.xlabel('Classes')
-plt.ylabel('Number of Samples')
+# height = list(classes)
+# bars = ('benign', 'ddos', 'backdoor', 'botnet', 'virus', 'trojan')
+# y_pos = np.arange(len(bars))
+# plt.bar(y_pos, height, color=['green', 'red', 'blue', 'yellow', 'orange', 'purple'])
+# plt.xticks(y_pos, bars)
+# plt.xlabel('Classes')
+# plt.ylabel('Number of Samples')
 # plt.savefig('distribution_count.png')
 # plt.show()
 
 X = data
 Y = target
 
-model = ExtraTreesClassifier(n_estimators=100)
+model = ExtraTreesClassifier()
 model.fit(X, Y)
 print(len(model.feature_importances_))
 
@@ -49,11 +53,10 @@ sorted_importance = sorted_feature_ranking.values()
 df_features = pd.DataFrame(sorted_feature_ranking.items(), columns=['features', 'importance'])
 
 n_features = X.shape[1]
-plt.figure(figsize=(18, 18))
+plt.figure(figsize=(60, 60))
 plt.barh(range(n_features), model.feature_importances_, align='edge')
 plt.yticks(np.arange(n_features), X.columns.values)
 plt.xlabel('Feature Importance')
 plt.ylabel('Features')
-plt.show()
-
-
+plt.savefig('feature_importance.png', bbox_inches='tight')
+# plt.show()
