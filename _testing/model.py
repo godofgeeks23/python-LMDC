4b02c112d23b2858f04bace43c2a1888b69afe0b
Aviral Srivastava <aviralji4@gmail.com>
2022-02-11 22:01:31 +0530

    Update model.py

--- _testing/model.py
+++ _testing/model.py
@@ -45,19 +45,19 @@ target = df[features[-1]]
 # plt.savefig('distribution_count.png')
 # plt.show()
 
-X = data
-Y = target
+# X = data
+# Y = target
 
-model = ExtraTreesClassifier()
-model.fit(X, Y)
+# model = ExtraTreesClassifier()
+# model.fit(X, Y)
 # print(len(model.feature_importances_))
 
-feature_ranking = {feature_value_pair[0]: feature_value_pair[1] for feature_value_pair in zip(features, model.feature_importances_)}
-sorted_feature_ranking = {k:v for k,v in sorted(feature_ranking.items(), key=lambda item: item[1], reverse=True)}
-sorted_features = sorted_feature_ranking.keys()
-sorted_importance = sorted_feature_ranking.values()
+# feature_ranking = {feature_value_pair[0]: feature_value_pair[1] for feature_value_pair in zip(features, model.feature_importances_)}
+# sorted_feature_ranking = {k:v for k,v in sorted(feature_ranking.items(), key=lambda item: item[1], reverse=True)}
+# sorted_features = sorted_feature_ranking.keys()
+# sorted_importance = sorted_feature_ranking.values()
 
-df_features = pd.DataFrame(sorted_feature_ranking.items(), columns=['features', 'importance'])
+# df_features = pd.DataFrame(sorted_feature_ranking.items(), columns=['features', 'importance'])
 
 # print(df_features)
 
@@ -80,28 +80,28 @@ df_features = pd.DataFrame(sorted_feature_ranking.items(), columns=['features', 
 # plt.show()
 
 # classifiers_accuracy = {}
-# data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.3, random_state=42)
+data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.3, random_state=42)
 
-# rf = RandomForestClassifier()
-# rf.fit(data_train, target_train)
-# pred = rf.predict(data_test)
+rf = RandomForestClassifier(n_estimators=500, max_depth=10, random_state=0)
+rf.fit(data_train, target_train)
+pred = rf.predict(data_test)
 
-# score = accuracy_score(target_test, pred, normalize=True)
-# print("F1 Score: {}%".format(f1_score(target_test, pred, average='macro')*100))
-# print("Accuracy: {}%".format(score*100))
+score = accuracy_score(target_test, pred, normalize=True)
+print("F1 Score: {}%".format(f1_score(target_test, pred, average='macro')*100))
+print("Accuracy: {}%".format(score*100))
 
 # filename = 'finalized_model_0.sav'
 # pickle.dump(rf, open(filename, 'wb'))
 # # classifiers_accuracy['Random Forest'] = score
 
-models = [RandomForestClassifier(), LogisticRegression(), LinearSVC(), MultinomialNB()]
-CV = 5
-cv_df = pd.DataFrame(index=range(CV * len(models)))
-entries = {"Model": [], "Mean Accuracy": []}
-for model in models:
-    model_name = model.__class__.__name__
-    accuracies = cross_val_score(model, data, target, scoring='accuracy', cv=CV)
-    entries['Model'].append(model_name)
-    entries['Mean Accuracy'].append(sum(accuracies) / float(len(accuracies)))
-plt.bar(entries['Model'], entries['Mean Accuracy'])
-plt.show()
+# models = [RandomForestClassifier(), LogisticRegression(), LinearSVC(), MultinomialNB()]
+# CV = 5
+# cv_df = pd.DataFrame(index=range(CV * len(models)))
+# entries = {"Model": [], "Mean Accuracy": []}
+# for model in models:
+#     model_name = model.__class__.__name__
+#     accuracies = cross_val_score(model, data, target, scoring='accuracy', cv=CV)
+#     entries['Model'].append(model_name)
+#     entries['Mean Accuracy'].append(sum(accuracies) / float(len(accuracies)))
+# plt.bar(entries['Model'], entries['Mean Accuracy'])
+# plt.show()
